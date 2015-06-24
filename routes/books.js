cd8b1fe8d3ac933f68f5cf9988a83ce0218c2779
Hogue <ajhogue6@gmail.com>
2015-06-24 14:47:16 -0400

    adds working get route that connects to the database and renders json on localhost for books

--- routes/books.js
+++ routes/books.js
@@ -1,31 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var Book = require('../models/books.js');
 
-router.get('/', function(req, res){
-  res.send("It worked!");
-});
-
-router.post('/', function(req, res){
-  res.send('post worked!');
-});
-
-router.route('/:book_id')
-  .all(function(req, res, next) {
-    contact_id = req.params.book_id;
-    next();
-  })
-  .get(function(req, res){
-    res.send("Hello " + book_id);
-  })
-  .post(function(req, res){
-    res.send('Post for contact ' + book_id);
-  })
-  .put(function(req, res){
-    res.send('Put for contact ' + book_id);
-  })
-  .delete(function(req, res){
-    res.send('book number ' + book_id + " " + "has been deleted!");
+router.get('/books', function(req, res) {
+  Book.find({},function(err, bookList) {
+    if(err) {
+      res.sendStatus(404);
+    }
+    res.json(bookList);
+    res.status(200);
   });
+});
+
+
+// router.get('/', function(req, res){
+//   res.send("It worked!");
+// });
+
+// router.post('/', function(req, res){
+//   res.send('post worked!');
+// });
+
+// router.route('/:book_id')
+//   .all(function(req, res, next) {
+//     contact_id = req.params.book_id;
+//     next();
+//   })
+//   .get(function(req, res){
+//     res.send("Hello " + book_id);
+//   })
+//   .post(function(req, res){
+//     res.send('Post for contact ' + book_id);
+//   })
+//   .put(function(req, res){
+//     res.send('Put for contact ' + book_id);
+//   })
+//   .delete(function(req, res){
+//     res.send('book number ' + book_id + " " + "has been deleted!");
+//   });
 
 
 module.exports = router;
