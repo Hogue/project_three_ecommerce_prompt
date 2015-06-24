93c6d6b053bc3a65592913d818d8d3235b2617a5
toomanyadverbs <lark.grimes@gmail.com>
2015-06-24 17:34:38 -0400

    Updates Orders routes and models

--- routes/orders.js
+++ routes/orders.js
@@ -1,14 +1,66 @@
 var express = require('express');
 var router = express.Router();
 
-// GET orders listing
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+router.get('/', jsonParser);
 
-router.get('/:order_id', function(req, res, next) {
-  res.send('');
+// GET individual orders listing
+
+router.get('/orders/:id', function(req, res, next) {
+  Order.find({
+    _id: req.params.id
+  }, function(error, order) {
+    if (error) {
+      console.log(error);
+      res.send('Error; cannot GET order by id')
+    } else {
+      res.json(order);
+      res.status(200);
+    }
+  });
 });
 
+//GET all orders by one user
+
+//POST new order
+router.post('/orders', function(req, res, next) {
+  Order.create({
+    user, books, date, purchased
+    user: req.body.user,
+    books: req.body.books,
+    date: req.body.date,
+    purchased: false
+  });
+});
+
+//PATCH (or is PUT better?) new individual book on an order
+
+apiRouter.patch('/orders/:id', function(req, res) {
+  Contact.findByIdAndUpdate(req.params.id, {
+    $set: req.body
+  }, function(error, orders) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(400);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+//DELETE order
+router.delete('/orders/:id', function(req, res) {
+  Order.remove({
+    _id: req.params.id
+  }, function(error) {
+    if (error) {
+      console.log(error);
+      res.sendStatus(400);
+    } else {
+      res.sendStatus(204);
+    }
+  });
+});
+
+//add jsonParser and bodyParser to each request
 
 module.exports = router;
