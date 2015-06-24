0ceda8b16bd37984b2236d4203d786881797bd78
Andrew Ellis <astoellis@gmail.com>
2015-06-24 17:00:05 -0400

    Modified app.js to send routes to orders & fixe books

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Order = require('../models/orders.js');
 
 // GET orders listing
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+router.get('/', function(req, res) {
+  Order.find({},function(err, orderList) {
+    if(err) {
+      res.sendStatus(404);
+    }
+    res.json(orderList);
+    res.status(200);
+  });
+});
 
 router.get('/:order_id', function(req, res, next) {
   res.send('');
