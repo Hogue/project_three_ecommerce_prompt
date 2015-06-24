a3c1b1fb47fc2064901276853c17fba96dfd7960
Hogue <ajhogue6@gmail.com>
2015-06-23 23:39:23 -0400

    adds get request

--- routes/books.js
+++ routes/books.js
@@ -5,4 +5,23 @@ router.get('/', function(req, res){
   res.send("It worked!");
 });
 
+router.route('/:book_id')
+  .all(function(req, res, next) {
+    contact_id = req.params.book_id;
+    next();
+  })
+  .get(function(req, res){
+    res.send("Hello " + book_id);
+  })
+  .post(function(req, res){
+    res.send('Post for contact ' + book_id);
+  })
+  .put(function(req, res){
+    res.send('Put for contact ' + book_id);
+  })
+  .delete(function(req, res){
+    res.send('book number ' + book_id + " " + "has been deleted!");
+  });
+
+
 module.exports = router;
