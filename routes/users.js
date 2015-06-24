4874bf5200b73c7bf8287be97fddcadb3fe73457
Andrew Ellis <astoellis@gmail.com>
2015-06-24 14:46:46 -0400

    Added basic user routes and schema
    * TODO: figure out authentication
    * TODO: Create dependent routes (e.g. users/:id/library)
    * Added a few files to the gitignore and connected to the db
        in the www file.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var User = require('../models/users.js');
+
+
+
+
+// router.all('/logout', function(req, res, next) {
+//   req.logout();
+//   res.redirect('/');
+// });
+
+router.get('/', jsonParser);
+router.get('/', function(req, res, next) {
+  User.find({}, function(err, usersList) {
+    if (err) {
+      console.log(err);
+      res.send('Error retrieving Contacts');
+    } else {
+      res.json(usersList);
+      res.status(200);
+    }
+  });
+  // res.json(req.user);
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  User.create({
+    nameFirst: req.body.nameFirst,
+    nameLast: req.body.nameLast,
+    email: req.body.email
+  });
 });
+
+router.post('/', function(req, res) {
+  res.send('Attempting to Post');
+});
+
 
 module.exports = router;
