47be39f585062fc72afda87bea79faa9ed55a46d
Hogue <ajhogue6@gmail.com>
2015-06-23 23:33:24 -0400

    ads books schema seed data

--- models/books.js
+++ models/books.js
@@ -1,79 +1,46 @@
-// Books
-// Books have reviews
-// Books — author, title, subtitle, format, pub_date, ISBN-13, ISBN-10, edition, condition, language, price, subject, description, reviews, keywords
-// Reviews — username, date, comment, approve_flag
+// Book
+// Title (String)
+// Author (String)
+// Price (Number)
+// Genre (String: Enumerated Type)
+// [Optional]
+// ISBN
+// Thumbnail
+// Length
 
   //require
-  var mongoose = require('mongoose');
+var mongoose = require('mongoose');
+require('./images.js');
 
-  //connect to server
-  // mongoose.connect('mongodb://localhost/amazon');
+var enumeratedGenreTypes = ['childrens', 'classics', 'fantasy', 'fiction', 'history', 'philosophy', 'programming', 'science fiction'];
 
-  //
-  enumeratedFormatTypes = ['digital', 'hardback', 'paperback'];
-  enumeratedConditionTypes = ['new','used'];
+// var imageSchema = new mongoose.Schema({
+//   url: {
+//     type: String,
+//     require: true
+//   }
+// });
 
-
-  var reviewSchema = new mongoose.Schema({
-    username: {
-      type: String,
-      required: true
-    },
-    date: {
-      type: String,
-      required: true
-    },
-    comment: {
-      type: String,
-      required: true
-    },
-    approval_flag: {
-      type: Boolean,
-      default: false
+var bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  price: Number,
+  genre: {
+    type: String,
+    required: true,
+    enum: {
+      values: enumeratedGenreTypes
     }
-  });
-
-  var bookSchema = new mongoose.Schema({
-    author: {
-      type: String,
-      required: true
-    },
-    title: {
-      type: String,
-      required: true
-    },
-    subtitle: String,
-    format: {
-      type: String,
-      required: true,
-      enum: {
-        values: enumeratedFormatTypes
-      }
-    },
-    pub_date: String,
-    ISBN_13: String,
-    ISBN_10: String,
-    edition: String,
-    condition: {
-      type: String,
-      required: true,
-      enum: {
-        values: enumeratedConditionTypes
-      }
-    },
-    language: String,
-    price: {
-      type: Number,
-      required: true
-    },
-    subject: String,
-    description: String,
-    keywords: String,
-    reviews: [reviewSchema]
-  });
-
-
-
+  },
+  isbn: String,
+  thumbnail: [mongoose.model('Image').schema]
+});
 
 //create model
 var Book = mongoose.model('Book', bookSchema);
