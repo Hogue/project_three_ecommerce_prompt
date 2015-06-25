f03e3eba4cb7c93e2a22ddb8897af6b84a1274c9
Andrew Ellis <astoellis@gmail.com>
2015-06-24 21:36:48 -0400

    seed-master-database.js now works, have to talk over removing other seed files soon

---
+++ scripts/seed-master-database.js
@@ -0,0 +1,70 @@
+var config = require('../config');
+var async = require('async');
+var mongoose = require('mongoose');
+
+mongoose.connect(config.mongo.dbUrl);
+var Book = require('../models/books.js');
+var User = require('../models/users.js');
+var Order = require('../models/orders.js');
+
+var removeBooks = function(done) {
+  Book.remove({}, done);
+};
+
+var removeUsers = function(done) {
+  User.remove({}, done);
+};
+
+var removeOrders = function(done) {
+  Order.remove({}, done);
+};
+
+var addBook = function(done) {
+  Book.create({
+    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+    author: 'Marvin Haverbeke',
+    price: 28.93,
+    genre: 'programming',
+    isbn: '978-1593275846',
+    thumbnail: [{
+      url: 'http://postimg.org/image/sd1pp6g61/'
+    }]
+  }, done);
+};
+
+var addUser = function(done) {
+  User.create({
+    email: 'drewkakes@gmail.com',
+    nameFirst: 'Andrew',
+    nameLast: 'Ellis'
+  }, done);
+};
+
+var addOrder = function(done) {
+  Order.create({
+    user: [],
+    books: [],
+    date: Date.now(),
+    sent: true
+  }, done);
+};
+
+async.series([
+    removeBooks,
+    addBook,
+    removeUsers,
+    addUser,
+    removeOrders,
+    addOrder
+  ],
+  // fire the function that will be invoked
+  // when the above functions are done
+  function(err) {
+    if (err) {
+      console.error(err);
+    }
+    // drop the db, because we don't need it anymore
+    mongoose.disconnect();
+  }
+
+);
