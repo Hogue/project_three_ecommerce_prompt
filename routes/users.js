b33e1873de6bbe2a5884ac6f3cad03e4977b1d28
Andrew Ellis <astoellis@gmail.com>
2015-06-24 22:34:04 -0400

    Added DELETE and PATCH routes for users * Got stuck trying to update a non-found user * Not sure how to validate if user exists, * Or how to tell mongoose to tell me NO if it can't find * TODO: Update only given fields in patch request

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,6 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var User = require('../models/users.js');
 
-
-
-
-// router.all('/logout', function(req, res, next) {
-//   req.logout();
-//   res.redirect('/');
-// });
-
 router.get('/', jsonParser);
 router.get('/', function(req, res, next) {
   User.find({}, function(err, usersList) {
@@ -23,15 +15,73 @@ router.get('/', function(req, res, next) {
       res.status(200);
     }
   });
-  // res.json(req.user);
 });
 
-/* GET users listing. */
-router.post('/', function(req, res, next) {
+router.get('/:id', jsonParser);
+router.get('/:id', function(req, res){
+  User.find({
+    _id: req.params.id
+  }, function(err, result){
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    } else {
+      // console.log('Result object: ' + result);
+      // console.log('Last name should be: ' + result['nameLast']);
+      res.json(result);
+      res.status(200);
+    }
+  });
+});
+
+/* Add user */
+router.post('/', function(req, res) {
   User.create({
     nameFirst: req.body.nameFirst,
     nameLast: req.body.nameLast,
-    email: req.body.email
+    email: req.body.email,
+    password: req.body.password
+  }, function(err, promise){
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    } else {
+      res.json(promise);
+      res.status(200);
+    }
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  User.remove({
+    _id: req.params.id
+  }, function(err){
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    } else {
+      res.send('User Deleted');
+    }
+  });
+});
+
+router.patch('/:id', function(req, res){
+  console.log(req);
+  User.findByIdAndUpdate(req.params.id, {
+    // email: req.body.email
+    nameFirst: req.body.nameFirst,
+    nameLast: req.body.nameLast,
+    email: req.body.email,
+    password: req.body.password
+  }, function(err, userFound){
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    } else {
+      console.log(userFound);
+      res.json(userFound);
+      // res.json(result);
+    }
   });
 });
 
