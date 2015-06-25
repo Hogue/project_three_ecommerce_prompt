d6d3ca1a905f10dcdcfa564e2c49c72f3a5c99c0
toomanyadverbs <lark.grimes@gmail.com>
2015-06-25 15:40:27 -0400

    Adds form to checkout.jade, and routes to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+//CLIENTSIDE INTERFACE
+
+//Note: Once authentication is implemented, add 'get Current User' to this function, so their User ID is stored in the browser for making payment
+//Also add their name to the page itself
+router.get('/checkout', function(req, res) {
+  res.render('checkout', {
+    userNameFull: "Beryl Berlioz",
+  });
 });
 
 module.exports = router;
