67528f57a67858fed72efca678975267dbfabf7b
Andrew Ellis <astoellis@gmail.com>
2015-06-26 12:40:04 -0400

    Made landing pages for admin interface to add and remove books * NOTE: When importing a file the path changes *   It took me way too long to figure that out * TODO: Make delete buttons work * TODO: Create home page landing * TODO: Create Individual Product Pages

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -3,15 +3,14 @@ $(document).ready(function() {
   // ========================
   // ADD BOOK FORM
   // ========================
-  $('#add-book').on('click', function(event) {
+  $('#add-book').on('submit', function(event) {
     event.preventDefault();
-    debugger;
     var book = {
-      title: $('input #book-title').val(),
-      author: $('input #book-author').val(),
-      price: $('input #book-price').val(),
-      genre: $('input #book-genre').val(),
-      isbn: $('input #book-isbn').val()
+      title: $('input#book-title').val(),
+      author: $('input#book-author').val(),
+      price: $('input#book-price').val(),
+      genre: $('input#book-genre').val(),
+      isbn: $('input#book-isbn').val()
     };
     $.ajax({
       method: 'POST',
@@ -19,13 +18,14 @@ $(document).ready(function() {
       data: JSON.stringify(book),
       contentType: "application/json; charset=utf-8"
     }).done(function(res) {
+      console.log(res);
       $('.books').append(res);
     });
   });
   // ========================
   // ADD USER FORM
   // ========================
-  $('#add-user').on('click', function(event) {
+  $('#add-user').on('submit', function(event) {
     event.preventDefault();
     if ($('input #user-password').val() === $('input #user-password-conf').val()) {
       var user = {
@@ -40,14 +40,15 @@ $(document).ready(function() {
         data: JSON.stringify(user),
         contentType: "application/json; charset=utf-8"
       }).done(function(res) {
-        $('.users').append(res);
+        console.log('Response from router: ' + res);
+        $('.users tbody').append(res);
       });
     }
   });
   // ========================
   // ADD ORDER FORM
   // ========================
-  $('#add-order').on('click', function(event) {
+  $('#add-order').on('submit', function(event) {
     event.preventDefault();
     var order = {
       date: Date.now(),
@@ -59,7 +60,28 @@ $(document).ready(function() {
       data: JSON.stringify(order),
       contentType: "application/json; charset=utf-8"
     }).done(function(res) {
-      $('.users').append(res);
+      $('.striped tbody').append(res);
     });
   });
+
+  var getType = function(itemString){
+    return itemString.split('-')[0];
+  };
+
+  $('a.delete').on('click', function(event) {
+    event.preventDefault();
+    var id = $(this).data('id');
+    delUrl = 'http://localhost:3000' + id;
+    var itemType = getType($(this).parent().class());
+    console.log(itemType);
+    debugger;
+    $.ajax({
+        url: delUrl,
+        method: 'DELETE',
+      })
+      .done(function() {
+        $(this).parent().remove();
+      });
+  });
+
 });
