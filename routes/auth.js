d10e392b188d56b6666736ecd790273570337b73
Hogue <ajhogue6@gmail.com>
2015-06-27 17:24:51 -0400

    adds login jade template, links it to /admin. also changed add-user to register, so now you can register a user, then login

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,34 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
+// REGISTER
+router.route('/register')
+  .get(function(req, res, next) {
+    res.render('register', {});
+  })
+  .post(function(req, res, next) {
+    Account.register(new Account({username: req.body.username}), req.body.password, function(err, account) {
+      if(err) {
+        return res.render('register', {account: account});
+      }
+
+      req.login(account, function(err) {
+        res.redirect('/contacts');
+      });
+    })
+  })
+
+// LOGIN
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {user: req.user});
+});
+
+router.all('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
+});
+
 router.post('/login', jsonParser);
 router.post('/login', function(req, res, next){
   console.log(req.body);
