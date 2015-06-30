b88741cc03bf56807eb0fcf1eb0cfbb6bfc348d3
Hogue <ajhogue6@gmail.com>
2015-06-30 12:09:44 -0400

    Fixes hostname url

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,4 +1,5 @@
 var App = App || {};
+var hostname = "arcane-escarpment-7767.herokuapp.com";
 
 $(document).ready(function() {
 
@@ -61,7 +62,7 @@ $(document).ready(function() {
     };
     $.ajax({
       method: 'POST',
-      url: 'http://' + config.hostname + '/nozama/books',
+      url: 'http://' + hostname + '/nozama/books',
       data: JSON.stringify(book),
       contentType: "application/json; charset=utf-8"
     }).done(function(res) {
@@ -85,7 +86,7 @@ $(document).ready(function() {
     };
     $.ajax({
         method: 'POST',
-        url: 'http://' + config.hostname + '/nozama/users',
+        url: 'http://' + hostname + '/nozama/users',
         data: JSON.stringify(user),
         contentType: "application/json; charset=utf-8"
       }).done(function(res) {
@@ -115,7 +116,7 @@ $(document).ready(function() {
     };
     $.ajax({
       method: 'POST',
-      url: 'http://' + config.hostname + '/nozama/orders',
+      url: 'http://' + hostname + '/nozama/orders',
       data: JSON.stringify(order),
       contentType: "application/json; charset=utf-8"
     }).done(function(res) {
@@ -143,7 +144,7 @@ $(document).ready(function() {
     var type = $(this).data('obj-type');
     var classSelector = '.' + type + '-item';
     var oid = $(this).parents(classSelector).data('id');
-    var delUrl = 'http://' + config.hostname + '/nozama/users' + type + 's/' + oid;
+    var delUrl = 'http://' + hostname + '/nozama/users' + type + 's/' + oid;
 
     $.ajax({
       url: delUrl,
